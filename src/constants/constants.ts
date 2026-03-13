d30333e5696afcbfe53861640ce11f4719f1332f
bluerosion <178355190+bluerosion@users.noreply.github.com>
2026-03-13 22:27:53 +0800

    Last Sync: 2026-03-13 22:27

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,7 +2,7 @@ export const PAGE_SIZE = 8;
 
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
-	AUTO_MODE = "dark";
+	AUTO_MODE = "auto";
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
