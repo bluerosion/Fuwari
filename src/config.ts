b44478f5a672d8fbf7a54dc687e22fca27761ce5
bluerosion <178355190+bluerosion@users.noreply.github.com>
2026-04-13 13:12:17 +0800

    Last Sync: 2026-04-13 13:12

--- src/config.ts
+++ src/config.ts
@@ -18,11 +18,11 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: true, // Display the credit text of the banner image
-			text: "睦头可爱捏", // Credit text to be displayed
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
